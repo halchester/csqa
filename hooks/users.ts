3f2d10dcf5c63cbbc5b9ea3c5099e099923e1f2a
Chester <chester.htooaunglinn@gmail.com>
2021-08-02 20:34:09 +0630

    feat: bio page and logging user out

--- hooks/users.ts
+++ hooks/users.ts
@@ -1,5 +1,6 @@
 import { useQuery } from "react-query";
 import axios from "../lib/api";
+import { useUserData } from "../store/userStore";
 
 const getCurrentUser = async () => {
   const res = await axios.get("/api/auth/currentUser");
@@ -8,6 +9,10 @@ const getCurrentUser = async () => {
 
 export const useUser = () => {
   const { data, refetch, isLoading } = useQuery("user", getCurrentUser);
+  const setUserData = useUserData((state) => state.setUserData);
+  if (!isLoading && data) {
+    setUserData(data);
+  }
   const user = data;
   return [user, isLoading, { refetch }];
 };
--- pages/bio.tsx
+++ pages/bio.tsx
@@ -1,35 +1,89 @@
-import { Badge, Box, Link, Stack, Text } from "@chakra-ui/react";
-import React from "react";
+import {
+  Badge,
+  Box,
+  Button,
+  Center,
+  Flex,
+  Link,
+  Stack,
+  Text,
+} from "@chakra-ui/react";
+import React, { useState } from "react";
 import { Layout } from "../components/layout/Layout";
 import { useUser } from "../hooks/users";
 import { Question } from "../types/common";
+import axios from "../lib/api";
+import { useRouter } from "next/dist/client/router";
 
 const BioPage = ({}) => {
-  // console.log(user);
-  // return (
-  //   <Layout>
-  //     <Text>{user.fullName}</Text>
-  //     <Text as='span' fontSize='sm' color='gray.500'>
-  //       {user.username} |
-  //     </Text>
-  //     <Text as='span' fontSize='sm' color='gray.500'>
-  //       &nbsp;{user.email}
-  //     </Text>
-  //     <Stack spacing='4' my='6'>
-  //       <Text fontSize='lg'>Questions you've uploaded</Text>
-  //       {user.questions.map((question: Question) => (
-  //         <>
-  //           <Text key={question.questionId} as='span'>
-  //             <Link href={`/question/${question.questionId}`}>
-  //               {question.title}
-  //             </Link>
-  //           </Text>
-  //           <Badge as='span'>{question.points}</Badge>
-  //         </>
-  //       ))}
-  //     </Stack>
-  //   </Layout>
-  // );
+  const [user, isLoading] = useUser();
+  const [loading, setLoading] = useState(false);
+  const router = useRouter();
+
+  const logoutUser = async () => {
+    setLoading(true);
+    try {
+      const response = await axios.delete("/api/auth");
+      console.log(response);
+      setLoading(false);
+      router.push("/");
+    } catch (err) {
+      console.log(err);
+      setLoading(false);
+    }
+  };
+
+  return (
+    <Layout>
+      {isLoading ? (
+        <Text>Please wait loading!</Text>
+      ) : (
+        <>
+          <Text>{user.fullName}</Text>
+          <Text as='span' fontSize='sm' color='gray.500'>
+            {user.username} |
+          </Text>
+          <Text as='span' fontSize='sm' color='gray.500'>
+            &nbsp;{user.email}
+          </Text>
+          <Stack spacing='4' my='6'>
+            <Text fontSize='lg' fontWeight='semibold'>
+              Questions you've uploaded
+            </Text>
+            {user.questions.map((question: Question) => (
+              <Flex
+                alignItems='center'
+                justifyContent='space-between'
+                key={question.questionId}
+              >
+                <Text key={question.questionId} as='span'>
+                  <Link href={`/question/${question.questionId}`}>
+                    {question.title}
+                  </Link>
+                </Text>
+                <Box>
+                  <Text as='span'>Points : </Text>
+                  <Badge ml='3' colorScheme='orange'>
+                    {question.points}
+                  </Badge>
+                </Box>
+              </Flex>
+            ))}
+          </Stack>
+        </>
+      )}
+      <Center>
+        <Button
+          size='sm'
+          colorScheme='red'
+          isLoading={loading}
+          onClick={logoutUser}
+        >
+          Log out
+        </Button>
+      </Center>
+    </Layout>
+  );
 };
 
 export default BioPage;
