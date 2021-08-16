a64d852ee518d1990a1669e5aa9fe2a5ce0ec32a
Chester <chester.htooaunglinn@gmail.com>
2021-08-16 17:53:34 +0630

    refactor: whole project

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,11 @@
 import * as React from "react";
-import { ChakraProvider } from "@chakra-ui/react";
-import { QueryClient, QueryClientProvider } from "react-query";
-import { AppProps } from "next/app";
+import {ChakraProvider} from "@chakra-ui/react";
+import {QueryClient, QueryClientProvider} from "react-query";
+import {AppProps} from "next/app";
 
 const queryClient = new QueryClient();
 
-function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+function MyApp({Component, pageProps}: AppProps): JSX.Element {
   return (
     <ChakraProvider>
       <QueryClientProvider client={queryClient}>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,80 +1,12 @@
-import * as React from "react";
-import { Question } from "../components/common/Question";
-import { Layout } from "../components/layout/Layout";
-import { QUESTIONS_PER_PAGE } from "../constants/common";
-import { Pagination } from "../components/common/Pagination";
-import { Question as QuestionT } from "../types/common";
-import { Text, Link } from "@chakra-ui/react";
-import { useUser } from "../hooks/users";
-import { useUserData } from "../store/userStore";
-import { GetServerSideProps } from "next";
-import axios from "../lib/api";
+import React from "react";
+import {Box, Text} from "@chakra-ui/react";
 
-interface IProps {
-  questions: QuestionT[];
-}
-
-const IndexPage = ({ questions }: IProps): JSX.Element => {
-  const [currPage, setCurrPage] = React.useState(1);
-  const setUserData = useUserData((state) => state.setUserData);
-  const [user] = useUser();
-
-  React.useEffect(() => {
-    if (user) {
-      setUserData(user);
-    }
-  }, []);
-
-  let indexOfLastQuestion = currPage * QUESTIONS_PER_PAGE;
-  let indexOfFirstQuestion = indexOfLastQuestion - QUESTIONS_PER_PAGE;
-  let currQuestions = questions?.slice(
-    indexOfFirstQuestion,
-    indexOfLastQuestion
-  );
-
-  const paginate = (num: number) => setCurrPage(num);
-
+const IndexPage = (): JSX.Element => {
   return (
-    <Layout>
-      {questions && (
-        <>
-          {currQuestions.map((question: QuestionT, idx: number) => (
-            <Question question={question} key={idx} />
-          ))}
-          {questions.length > 8 ? (
-            <Pagination totalQuestions={questions.length} paginate={paginate} />
-          ) : null}
-        </>
-      )}
-      {questions.length === 0 ? (
-        <>
-          <Text align='center'>There are no questions at the moment</Text>
-          <Text align='center' textDecoration='underline'>
-            <Link href='/question/new'>Create one now!</Link>
-          </Text>
-        </>
-      ) : null}
-    </Layout>
+    <Box>
+      <Text>hi</Text>
+    </Box>
   );
 };
 
 export default IndexPage;
-
-export const getServerSideProps: GetServerSideProps = async () => {
-  const data = await axios.get(`/api/question`);
-
-  if (!data) {
-    return {
-      redirect: {
-        destination: "/",
-        permanent: false,
-      },
-    };
-  }
-
-  return {
-    props: {
-      questions: data.data.data,
-    },
-  };
-};
